c959ac814254e2f6afe977d55bfe80d5325da57d
Felix S. Klock II <pnkfelix@pnkfx.org>
2014-02-24 07:58:46 +0100

    Code cleanup, factored shared draw/erase code into closure.

--- demo.rs
+++ demo.rs
@@ -11,20 +11,23 @@ pub extern "C" fn SDL_main(argc: int, argv: **u8) {
             let screen = sdl::video::set_video_mode
                 (800, 600, 32, [sdl::video::HWSurface], [sdl::video::DoubleBuf])
                 .ok().expect("Failed to set_video_mode");
+
+            let purple = sdl::video::RGB(128, 0, 128);
+            let black = sdl::video::RGB(0, 0, 0);
+            let square = |x:u16,y:u16,w,color|
+                { screen.fill_rect(Some(sdl::Rect {
+                            x: x as i16, y: y as i16, w: w, h: w }), color)
+                  || fail!("error on fill_rect attempt"); };
+
             'main : loop {
                 'events : loop {
                     match sdl::event::poll_event() {
                         sdl::event::QuitEvent |
                         sdl::event::KeyEvent(sdl::event::EscapeKey, _, _, _) => break 'main,
                         sdl::event::NoEvent => break 'events,
-                        sdl::event::MouseMotionEvent(ref state, x, y, _xrel, _yrel) if state.len() > 0 =>
-                            { screen.fill_rect(Some(sdl::Rect { x: x as i16, y: y as i16, w: 30, h: 30 }),
-                                               sdl::video::RGB(128, 0, 128)) || fail!("error on fill_rect attempt"); },
-                        
-                        sdl::event::MouseMotionEvent(ref state, x, y, _xrel, _yrel) if state.len() == 0 =>
-                            { screen.fill_rect(Some(sdl::Rect { x: x as i16, y: y as i16, w: 3, h: 3 }),
-                                               sdl::video::RGB(0, 0, 0)) || fail!("error on fill_rect attempt"); },
-                        
+                        sdl::event::MouseMotionEvent(ref state, x, y, _xrel, _yrel) if state.len() > 0 => square(x, y, 30, purple),
+
+                        sdl::event::MouseMotionEvent(ref state, x, y, _xrel, _yrel) if state.len() == 0 => square(x, y, 3, black),
                         e => {
                             println!("e: {:?}", e);
                         }
