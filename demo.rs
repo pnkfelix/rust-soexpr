318acdce015aaf89cb3ce0bef6fe212225e56888
Felix S. Klock II <pnkfelix@pnkfx.org>
2014-02-24 08:24:28 +0100

    Code cleanup: reindent default demo.

--- demo.rs
+++ demo.rs
@@ -28,37 +28,37 @@ fn testsprite() {
 }
 
 fn default() {
-            sdl::init([sdl::InitVideo, sdl::InitTimer]);
-            sdl::wm::set_caption("rust-sdl demo - video", "rust-sdl");
-            let screen = vid::set_video_mode
-                (800, 600, 32, [vid::HWSurface], [vid::DoubleBuf])
-                .ok().expect("Failed to set_video_mode");
+    sdl::init([sdl::InitVideo, sdl::InitTimer]);
+    sdl::wm::set_caption("rust-sdl demo - video", "rust-sdl");
+    let screen = vid::set_video_mode
+        (800, 600, 32, [vid::HWSurface], [vid::DoubleBuf])
+        .ok().expect("Failed to set_video_mode");
 
-            let purple = vid::RGB(128, 0, 128);
-            let black = vid::RGB(0, 0, 0);
-            let square = |x:u16, y:u16, width, color| {
-                let w = width;
-                let r = sdl::Rect { x: x as i16, y: y as i16, w: w, h: w };
-                screen.fill_rect(Some(r), color)
-                    || fail!("error on fill_rect attempt");
-            };
+    let purple = vid::RGB(128, 0, 128);
+    let black = vid::RGB(0, 0, 0);
+    let square = |x:u16, y:u16, width, color| {
+        let w = width;
+        let r = sdl::Rect { x: x as i16, y: y as i16, w: w, h: w };
+        screen.fill_rect(Some(r), color)
+            || fail!("error on fill_rect attempt");
+    };
 
-            'main : loop {
-                'events : loop {
-                    match evt::poll_event() {
-                        QuitEvent | KeyEvent(evt::EscapeKey, _, _, _)
-                            => break 'main,
-                        NoEvent
-                            => break 'events,
-                        Motion(ref state, x, y, _xrel, _yrel) if state.len() > 0
-                            => square(x, y, 30, purple),
-                        Motion(ref state, x, y, _xrel, _yrel) if state.len() == 0
-                            => square(x, y, 3, black),
-                        e
-                            => println!("e: {:?}", e),
-                    }
-                }
-                screen.flip();
+    'main : loop {
+        'events : loop {
+            match evt::poll_event() {
+                QuitEvent | KeyEvent(evt::EscapeKey, _, _, _)
+                    => break 'main,
+                NoEvent
+                    => break 'events,
+                Motion(ref state, x, y, _xrel, _yrel) if state.len() > 0
+                    => square(x, y, 30, purple),
+                Motion(ref state, x, y, _xrel, _yrel) if state.len() == 0
+                    => square(x, y, 3, black),
+                e
+                    => println!("e: {:?}", e),
             }
-            println!("Hello World");
+        }
+        screen.flip();
+    }
+    println!("Hello World");
 }
