2a82def1a34a8b916513cc56b2abcb48c84996b8
Felix S. Klock II <pnkfelix@pnkfx.org>
2014-02-25 00:30:01 +0100

    Revised code so that the moving shapes can have independent velocities.

--- tests/soe.rs
+++ tests/soe.rs
@@ -2,7 +2,7 @@ use sdl;
 use vid = sdl::video;
 use evt = sdl::event;
 
-static width:int = 640;
+static width:int  = 640;
 static height:int = 480;
 static video_bpp: int = 32;
 
@@ -61,6 +61,31 @@ static video_flags : (&'static [vid::SurfaceFlag],
                       &'static [vid::VideoFlag])   = (&[vid::HWSurface],
                                                       &[vid::AnyFormat]);
 
+struct Moving<T> {
+    obj: T,
+    x: int,
+    y: int,
+    dx: int,  // per tick
+    dy: int,  // per tick
+}
+
+impl<T> Moving<T> {
+    fn new(obj: T, (x,y): (int,int), (dx,dy): (int, int)) -> Moving<T> {
+        Moving { obj: obj, x: x, y: y, dx: dx, dy: dy }
+    }
+
+    fn tick(&mut self) {
+        let x2 = self.x + self.dx;
+        let y2 = self.y + self.dy;
+        if x2 < 0 || x2 + 100 > width { self.dx = -self.dx; } else { self.x  = x2; }
+        if y2 < 0 || y2 + 100 > height { self.dy = -self.dy; } else { self.y  = y2; }
+    }
+
+    fn next(self) -> Moving<T> {
+        Moving { x: self.x + self.dx, y: self.y + self.dy, .. self }
+    }
+}
+
 pub fn main(invoker: &str, args: &[~str]) {
     println!("running {} args: {}", invoker, args);
 
@@ -80,23 +105,47 @@ pub fn main(invoker: &str, args: &[~str]) {
     let shape  = new_circle(100, 100, 50, (0xF0u8, 0x20u8, 0x30u8));
     let shape2 = new_circle(100, 100, 50, (0x10u8, 0xA0u8, 0xB0u8));
 
-    // shape.set_alpha([vid::SrcAlpha], 128u8);
-    // shape2.set_alpha([vid::SrcAlpha], 128u8);
+    let mut shape = Moving::new(shape, (300, 0), (1, 2));
+    let mut shape2 = Moving::new(shape2, (0, 20), (-4, 3));
 
     let (mut x, mut y) = (0i16, 0i16);
+
+    let mut frames = 0;
+    let then = sdl::get_ticks();
+
     loop {
+        frames += 1;
+        if frames > 100 { break; }
         x = x % width as i16;
         y = y % height as i16;
-        screen.clear();
-        screen.blit_at(shape2, x + 50, y - 50);
-        screen.blit_at(shape, x, y);
+
+        // screen.clear();
+
+        screen.fill_rect(Some(sdl::Rect { x: shape2.x as i16,
+                                          y: shape2.y as i16,
+                                          w: 100,
+                                          h: 100 }), vid::RGB(0,0,0));
+        screen.fill_rect(Some(sdl::Rect { x: shape.x as i16,
+                                          y: shape.y as i16,
+                                          w: 100,
+                                          h: 100 }), vid::RGB(0,0,0));
+        shape.tick();
+        shape2.tick();
+        screen.blit_at(shape2.obj, shape2.x as i16, shape2.y as i16);
+        screen.blit_at(shape.obj, shape.x as i16, shape.y as i16);
+
         screen.flip();
         match evt::poll_event() {
             evt::KeyEvent(evt::EscapeKey, _, _, _) | evt::QuitEvent => break,
             evt::NoEvent => {}
             _ => {}
         }
-        x += 1;
-        y += 1;
+    }
+
+    // Print out some timing information
+    let now = sdl::get_ticks();
+    if now > then {
+        println!("{:2.2f} frames per second\n",
+                 (frames as f64 * 1000.0)/(now-then) as f64);
     }
 }
