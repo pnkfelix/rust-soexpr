53c88fa1612e5e3a12594cb8561b1d901b539768
Felix S. Klock II <pnkfelix@pnkfx.org>
2014-02-24 08:05:40 +0100

    Code cleanup: Add `use` decls to shorten paths, factored code for readability.

--- demo.rs
+++ demo.rs
@@ -3,34 +3,42 @@
 extern crate native;
 extern crate sdl;
 
+use evt = sdl::event;
+use vid = sdl::video;
+use sdl::event::{QuitEvent, KeyEvent, NoEvent};
+use Motion = sdl::event::MouseMotionEvent;
+
 #[no_mangle]
 pub extern "C" fn SDL_main(argc: int, argv: **u8) {
     native::start(argc, argv, proc() {
             sdl::init([sdl::InitVideo, sdl::InitTimer]);
             sdl::wm::set_caption("rust-sdl demo - video", "rust-sdl");
-            let screen = sdl::video::set_video_mode
-                (800, 600, 32, [sdl::video::HWSurface], [sdl::video::DoubleBuf])
+            let screen = vid::set_video_mode
+                (800, 600, 32, [vid::HWSurface], [vid::DoubleBuf])
                 .ok().expect("Failed to set_video_mode");
 
-            let purple = sdl::video::RGB(128, 0, 128);
-            let black = sdl::video::RGB(0, 0, 0);
-            let square = |x:u16,y:u16,w,color|
-                { screen.fill_rect(Some(sdl::Rect {
-                            x: x as i16, y: y as i16, w: w, h: w }), color)
-                  || fail!("error on fill_rect attempt"); };
+            let purple = vid::RGB(128, 0, 128);
+            let black = vid::RGB(0, 0, 0);
+            let square = |x:u16, y:u16, width, color| {
+                let w = width;
+                let r = sdl::Rect { x: x as i16, y: y as i16, w: w, h: w };
+                screen.fill_rect(Some(r), color)
+                    || fail!("error on fill_rect attempt");
+            };
 
             'main : loop {
                 'events : loop {
-                    match sdl::event::poll_event() {
-                        sdl::event::QuitEvent |
-                        sdl::event::KeyEvent(sdl::event::EscapeKey, _, _, _) => break 'main,
-                        sdl::event::NoEvent => break 'events,
-                        sdl::event::MouseMotionEvent(ref state, x, y, _xrel, _yrel) if state.len() > 0 => square(x, y, 30, purple),
-
-                        sdl::event::MouseMotionEvent(ref state, x, y, _xrel, _yrel) if state.len() == 0 => square(x, y, 3, black),
-                        e => {
-                            println!("e: {:?}", e);
-                        }
+                    match evt::poll_event() {
+                        QuitEvent | KeyEvent(evt::EscapeKey, _, _, _)
+                            => break 'main,
+                        NoEvent
+                            => break 'events,
+                        Motion(ref state, x, y, _xrel, _yrel) if state.len() > 0
+                            => square(x, y, 30, purple),
+                        Motion(ref state, x, y, _xrel, _yrel) if state.len() == 0
+                            => square(x, y, 3, black),
+                        e
+                            => println!("e: {:?}", e),
                     }
                 }
                 screen.flip();
