137635fc9dc8437a2d959acf127c166285e27833
Felix S. Klock II <pnkfelix@pnkfx.org>
2014-04-01 09:22:50 +0200

    Baby steps towards using rust-sdl2 instead of rust-sdl.

--- Makefile
+++ Makefile
@@ -1,21 +1,11 @@
-default: soe # do-demo
+default: hello # soe # do-demo
 
 DEMO_SRC=demo.rs
 DEMO_DEPS=$(wildcard *.rs tests/*.rs)
 DEMO=$(shell rustc --crate-file-name $(DEMO_SRC))
 
-SDL_1_2_INSTALL_DBGNOPT=$(HOME)/opt/sdl-release-1.2.15-dbg-nopt
-SDL_1_2_INSTALL_OPT=$(HOME)/opt/sdl-release-1.2.15
-SDL_1_2_ROOT=$(HOME)/Dev/Rust/rust-sdl/SDL-mirror
-SDL_LIBDIR_DBGNOPT=$(SDL_1_2_INSTALL_DBGNOPT)/lib
-SDL_LIBDIR_OPT=$(SDL_1_2_INSTALL_OPT)/lib
-SDL_INCLUDEDIR_DBGNOPT=$(SDL_1_2_INSTALL_DBGNOPT)/include/SDL
-SDL_INCLUDEDIR_OPT=$(SDL_1_2_INSTALL_OPT)/include/SDL
-
 # RUST_SDL_LIBDIR=$(HOME)/Dev/Rust/rust-sdl/objdir-opt
 RUST_SDL_LIBDIR=$(HOME)/opt/rustlibs
-
-SDL_MAIN_M=$(SDL_1_2_ROOT)/src/main/macosx/SDLMain.m
 
 #do-demo: $(DEMO)-dbg $(DEMO)
 do-demo: $(DEMO)-dbg
@@ -25,11 +15,14 @@ do-demo: $(DEMO)-dbg
 soe: $(DEMO)-dbg
 	./$< $@
 
+hello: $(DEMO)-dbg
+	./$< $@
+
 testsprite: $(DEMO)-dbg $(DEMO)
 	./$< $@
 
 $(DEMO)-dbg: $(DEMO_SRC) $(DEMO_DEPS)
-	rustc -o $@ -g $< -L$(RUST_SDL_LIBDIR) -L$(SDL_LIBDIR_DBGNOPT) -C link-args=" -I$(SDL_INCLUDEDIR_DBGNOPT) -framework CoreFoundation -framework CoreGraphics -framework AppKit $(SDL_MAIN_M)  "
+	rustc -o $@ -g $< -L$(RUST_SDL_LIBDIR) -C link-args=" -framework CoreFoundation -framework CoreGraphics -framework AppKit $(SDL_MAIN_M)  "
 
 $(DEMO): $(DEMO_SRC) $(DEMO_DEPS)
-	rustc -O -o $@ -g $< -L$(RUST_SDL_LIBDIR) -L$(SDL_LIBDIR_OPT) -C link-args=" -I$(SDL_INCLUDEDIR_OPT) -framework CoreFoundation -framework CoreGraphics -framework AppKit $(SDL_MAIN_M)  "
+	rustc -O -o $@ -g $< -L$(RUST_SDL_LIBDIR) -C link-args=" -framework CoreFoundation -framework CoreGraphics -framework AppKit $(SDL_MAIN_M)  "
--- demo.rs
+++ demo.rs
@@ -1,56 +1,97 @@
-#[no_main]; // provided by the SDLMain.m we'll link in.
+#![allow(unused_imports)] // don't warn me about this right now
 
 extern crate native;
-extern crate sdl;
+extern crate rand;
+
+extern crate sdl = "sdl2";
 
 use std::os;
 
 use evt = sdl::event;
 use vid = sdl::video;
-use sdl::event::{QuitEvent, KeyEvent, NoEvent};
+use pix = sdl::pixels;
+use key = sdl::keycode;
+use rend = sdl::render;
+use surf = sdl::surface;
+use rect = sdl::rect;
+use sdl::event::{QuitEvent, NoEvent};
+use KeyEvent = sdl::event::KeyUpEvent;
+use sdl::rect::{Rect};
 use Motion = sdl::event::MouseMotionEvent;
 
-#[no_mangle]
-pub extern "C" fn SDL_main(argc: int, argv: **u8) {
-    native::start(argc, argv, proc() {
-            let args = os::args();
-            if args.len() >= 2 {
-                dispatch(args[0], args[1], args.slice_from(2));
-            } else {
-                default();
-            }
-        });
+#[start]
+pub fn start(argc: int, argv: **u8) -> int {
+    native::start(argc, argv, main)
+}
+
+#[main]
+fn main() {
+    let args = os::args();
+    if args.len() >= 2 {
+        dispatch(args[0], args[1], args.slice_from(2));
+    } else {
+        default();
+    }
 }
 
 fn dispatch(driver: &str, variant: &str, args: &[~str]) {
     let invoker = format!("{} {}", driver, variant);
     match variant {
+/*
         "testsprite"
             => tests::testsprite::main(invoker, args),
         "soe"
             => tests::soe::main(invoker, args),
+*/
+        "hello"
+            => hello().unwrap(),
         _otherwise
             => default(),
     }
 }
 
 mod tests {
-    pub mod testsprite;
-    pub mod soe;
+//    pub mod testsprite;
+//    pub mod soe;
 }
 
+fn hello() -> Result<(), ~str> {
+    static SCREEN_WIDTH:int = 640;
+    static SCREEN_HEIGHT:int = 480;
+    try!(sdl::init([sdl::InitEverything]));
+    let win = try!(
+        vid::Window::new("Hello World", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, [vid::Shown]));
+    let ren = try!(rend::Renderer::from_window(
+        win, rend::DriverAuto, [rend::Accelerated, rend::PresentVSync]));
+    let file = Path::new("hello.bmp");
+    let bmp = try!(surf::Surface::from_bmp(&file));
+    let tex = try!(ren.create_texture_from_surface(bmp));
+    fn renderTexture(tex: &rend::Texture, ren: &rend::Renderer, x: i32, y: i32) -> Result<(), ~str> {
+        let q = try!(tex.query());
+        let dst = rect::Rect::new(x, y, q.width, q.height);
+        ren.copy(tex, None, Some(dst))
+    }
+    ren.clear();
+    ren.copy(tex, None, None);
+    ren.present();
+    sdl::timer::delay(2000);
+    Ok(())
+}
+
+#[cfg(not(with_default))]
+fn default() { unimplemented!() }
+#[cfg(with_default)]
 fn default() {
     sdl::init([sdl::InitVideo, sdl::InitTimer]);
-    sdl::wm::set_caption("rust-sdl demo - video", "rust-sdl");
-    let screen = vid::set_video_mode
-        (800, 600, 32, [vid::HWSurface], [vid::DoubleBuf])
+    let screen = vid::Window::new("rust-sdl demo - video",
+        vid::PosUndefined, vid::PosUndefined, 800, 600, [])
         .ok().expect("Failed to set_video_mode");
 
-    let purple = vid::RGB(128, 0, 128);
-    let black = vid::RGB(0, 0, 0);
+    let purple = pix::RGB(128, 0, 128);
+    let black = pix::RGB(0, 0, 0);
     let square = |x:u16, y:u16, width, color| {
         let w = width;
-        let r = sdl::Rect { x: x as i16, y: y as i16, w: w, h: w };
+        let r = Rect { x: x as i32, y: y as i32, w: w, h: w };
         screen.fill_rect(Some(r), color)
             || fail!("error on fill_rect attempt");
     };
@@ -58,7 +99,7 @@ fn default() {
     'main : loop {
         'events : loop {
             match evt::poll_event() {
-                QuitEvent | KeyEvent(evt::EscapeKey, _, _, _)
+                QuitEvent | KeyEvent(key::EscapeKey, _, _, _)
                     => break 'main,
                 NoEvent
                     => break 'events,
